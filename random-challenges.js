bb77a0d5c719cb267988d8829535f20f6952ddfe
David Haugen <haugen.dj@gmail.com>
2019-07-08 13:14:25 -0500

    complete findIndex function

--- random-challenges.js
+++ random-challenges.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function maximumThirdEdgeOfTriangle(firstEdge, secondEdge){
-    return firstEdge + secondEdge -1;
+  return firstEdge + secondEdge -1;
+}
+
+function findIndex(array, itemToFind){
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === itemToFind){
+      return i;
+    }
+  }
+  return -1;
 }
 
 console.log(maximumThirdEdgeOfTriangle(8,10));
